08bae61428261af27bca1f9839f8ac9a6266d789
Khurram Virani <kvirani@gmail.com>
2015-10-20 10:48:08 -0700

    implement auth (live coding for oct19 van cohort)

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,18 @@
+
+helpers do
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    if cookies[:user_id]
+      User.find(cookies[:user_id])
+    end
+  end
+end
+
+
 get '/' do
   @posts = Post.order 'created_at DESC'
   erb :index
@@ -8,13 +23,40 @@ get '/signup' do
   erb :signup
 end
 
+post '/signup' do
+  @user = User.new params.slice('email', 'username', 'password', 'avatar_url')
+  if @user.save
+    cookies[:user_id] = @user.id
+    redirect to("/")
+  else
+    erb :"posts/new"
+  end
+end
+
+
 get '/login' do
   erb :login
+end
+
+post '/login' do 
+  @user = User.find_by(username: params[:username], password: params[:password])
+  if @user
+    cookies[:user_id] = @user.id
+    redirect to('/')
+  else
+    @error_messages = ['Invalid username or password, yo!']
+    erb :login
+  end
 end
 
 get '/posts/new' do
   @post = Post.new
   erb :"posts/new"
+end
+
+put '/posts/:id' do
+  @post = Post.find(params[:id])
+  halt unless logged_in? && @post.user == current_user
 end
 
 get '/posts/:id' do
@@ -32,6 +74,7 @@ post '/posts' do
 end
 
 post '/comments' do
+  halt(400) unless logged_in?
   Comment.create params.slice('text', 'post_id', 'user_id')
   redirect back
 end
